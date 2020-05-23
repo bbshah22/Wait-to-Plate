3a193b9e5325bdf1642c580749bdea1faa140e55
Bhuvni Shah <bhuvni.shah@gmail.com>
2020-05-22 22:31:57 -0500

    hi

---
+++ Wait to Plate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wait to Plate
+//
+//  Created by Bhuvni Shah on 5/22/20.
+//  Copyright © 2020 Bhuvni Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
